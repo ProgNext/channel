b51f2fcd93e8c5f57d6c23e46195c156fe678f1d
Andrey <aws110615@gmail.com>
2021-10-30 02:24:41 +0500

    Create user's tags of template

--- channelsite/news/views.py
+++ channelsite/news/views.py
@@ -5,21 +5,17 @@ from .models import *
 
 def index(request):
     news = News.objects.order_by('-created_at')
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'List news',
-        'categories': categories
     }
     return render(request, 'news/index.html', context=context)
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {
         'news': news,
-        'categories': categories,
         'category': category,
     }
     return render(request, template_name='news/category.html', context=context)
